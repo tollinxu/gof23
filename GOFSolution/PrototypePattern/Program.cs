b88836681974be77a6b1a496ae6bd5041c881fc0
excellentony@gmail.com <tollinxu@gmail.com>
2019-05-10 23:42:27 +1200

    prototype pattern

---
+++ GOFSolution/PrototypePattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var felix = new Resume("felix");
+            felix.Age = 30;
+            felix.University = "Shandong University";
+
+            var anita = (Resume)felix.Clone();
+            anita.Name = "Anita";
+
+            Console.WriteLine(felix);
+            Console.WriteLine(anita);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GOFSolution/PrototypePattern/Resume.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypePattern
+{
+    public class Resume : ICloneable
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string University { get; set; }
+
+        public Resume(string name)
+        {
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            return "this is " + this.Name + $"'s resume. His age is {Age}, and graduated from {University}";
+        }
+
+        public object Clone()
+        {
+            return (object)this.MemberwiseClone();
+        }
+    }
+}
